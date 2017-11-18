1f11315ec9bf7cd3b75f281fe644f44e1858462d
Leonardo Wellausen <lwellausen@gmail.com>
2017-11-18 19:59:57 -0200

    fixes

--- Layer.py
+++ Layer.py
@@ -9,4 +9,5 @@ class Layer:
     def __init__(self, neuron_num, layer_type):
         self.layer_type = layer_type
         self.neurones = [Neurone(1.0, neuron_num)]
-        self.neurones = [self.neurones.append(Neurone(0, neuron_num)) for i in range(0, neuron_num)]
+        for i in range(0, neuron_num):
+            self.neurones.append(Neurone(0, neuron_num))
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,9 @@ from Layer import Layer
 
 
 class NeuralNetwork:
-    def __init__(self, layer_num, neuronsPerLayer_num, attributes_num):
-        self.layers = [Layer(neuronsPerLayer_num, 0)]
-        self.layers = [self.layers.append(Layer(neuronsPerLayer_num, 1)) for i in range(0, layer_num)]
-        self.layers = [Layer(neuronsPerLayer_num, 2)]
+    def __init__(self, layer_num, neuronsPerLayer_num, attributes_num, classes_num):
+        self.classes_num = classes_num   #  maybe there's a more complicated calculation here
+        self.layers = [Layer(attributes_num, 0)]
+        for i in range(0, layer_num):
+            self.layers.append(Layer(neuronsPerLayer_num, 1))
+        self.layers.append(Layer(self.classes_num, 2))
