1fe59c07d1d96e0ebb7a7c696552dc95f6899c75
Leonardo Wellausen <lwellausen@gmail.com>
2017-11-18 16:25:18 -0200

    first commit

---
+++ Neurone.py
@@ -0,0 +1,11 @@
+import random, math, pickle
+
+class Neurone:
+    def __init__(self, ativ):
+        self.ativ = ativ
+
+
+class Conection:
+    def __init__(self, weight):
+        self.weight = weight
+        
