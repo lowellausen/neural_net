72843d5bbcb3a5765904bac50ad09243eacbbc1c
NORMANDY\renan <renanghp@gmail.com>
2017-11-18 20:37:49 -0200

    created delta functions

--- Neurone.py
+++ Neurone.py
@@ -6,3 +6,17 @@ class Neurone:
         self.ativ = ativ
         self.delta = 0.0
         self.weights = [random.uniform(0.0, 1.0) for i in range(0,neurones_num)]
+		
+		
+def delta(self, deltaListPreviousLayer):
+	for i,delta in enumerate(deltaListPreviousLayer):
+		deltaNeuron += self.weights[i]*delta*ativ*(1-ativ)
+	self.delta = deltaNeuron
+	
+	return
+	
+def deltaOutput(self, foreseenClass):
+
+	self.detal = ativ - foreseenClass
+	
+	return
