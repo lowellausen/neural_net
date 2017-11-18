c2cf1032bd02bf8173695fc2e5e7464756cf27a0
Leonardo Wellausen <lwellausen@gmail.com>
2017-11-18 18:54:48 -0200

    n commit

--- Layer.py
+++ Layer.py
@@ -1,1 +1,8 @@
-import Neurone
+from Neurone import Neurone
+
+
+class Layer:
+    def __init__(self, neuron_num):
+        self.neurones = [Neurone(1, neuron_num)]
+        self.neurones = [self.neurones.append(Neurone(0, neuron_num)) for i in range(0, neuron_num)]
+        
