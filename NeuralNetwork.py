17bd67d522008a70833a4d0bd72b5f1096bc9f90
NORMANDY\renan <renanghp@gmail.com>
2017-11-18 19:21:23 -0200

    included the creation of all 3 types of layer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,8 @@
-from Neurone import Neurone
-
+from Layer import Layer
 class NeuralNetwork:
-	def __init__(self, layer_num, neuronsPerLayer_num):
+	def __init__(self, layer_num, neuronsPerLayer_num, attributes_num):
+		self.layers = [Layer(neuronsPerLayer_num, 0)]
+		
+		self.layers = [self.layers.append(Layer(neuronsPerLayer_num, 1)) for i in range(0, layer_num)]
 	
-	
-	
+		self.layers = [Layer(neuronsPerLayer_num, 2)]
