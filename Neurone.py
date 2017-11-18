c1ad15e9e4552f4fd639bbdf41f27400c2e6ce0a
Leonardo Wellausen <lwellausen@gmail.com>
2017-11-18 18:33:05 -0200

    third commit

--- Neurone.py
+++ Neurone.py
@@ -3,4 +3,4 @@ import random, math, pickle
 class Neurone:
     def __init__(self, ativ, neurones_nbr):
         self.ativ = ativ
-        self.weights = [random.uniform(0.0, 1.0) for neuron in neurones_nbr]
+        self.weights = [random.uniform(0.0, 1.0) for i in range(0,neurones_nbr)]
