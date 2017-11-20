da9679f95fb2aef57216187c4bc6fd3b88f76cd3
Leonardo Wellausen <lwellausen@gmail.com>
2017-11-20 19:14:46 -0200

    err func missing

--- second_try/Layer.py
+++ second_try/Layer.py
@@ -12,11 +12,11 @@ def sigmoid(x):
 
 
 class Layer:
-    def __init__(self, type, neuron_num, inputs_num):
+    def __init__(self, type, neuron_num, inputs_num, alpha):
+        self.alpha = alpha
         self.neuron_num = neuron_num
         self.inputs_num = inputs_num
         self.ins_per_neuron = inputs_num/neuron_num
-        self.outs_per_neuron =
         self.type = type
         #  inicializa pesos com valor randomico,
         if self.type is input_layer:
@@ -28,10 +28,12 @@ class Layer:
         if self.type is output_layer:
             #  output layer tem número de saídas pesos * neurons na hidden layer anterior, randomicos
             self.weights = [random.uniform(-random_limit, random_limit) for i in range(self.inputs_num)]
-        #  mais um peso de bias para cada neurônio, caso não seja output layer
-        if self.type is not output_layer:
-            self.bias_weights = [random.uniform(-random_limit, random_limit) for i in range(neuron_num)]
-        self.gradients = [0.0 for i in range(self.neuron_num)]
+        #  mais um peso de bias para cada neurônio, caso não seja input layer
+        if self.type is not input_layer:
+            self.bias_weights = [random.uniform(-random_limit, random_limit) for i in range(self.neuron_num)]
+        else:
+            self.bias_weights = []
+        self.gradients = [0.0 for i in range(self.inputs_num)]
         self.deltas = [0.0 for i in range(neuron_num)]
         #  valores de ativação de cada neurônio, saídas
         self.activia = [0.0 for i in range(neuron_num)]
@@ -52,6 +54,8 @@ class Layer:
             for j in range(self.ins_per_neuron):
                 index = (i*(self.neuron_num-1))+j  # DANGER cálculo para acessar os índices certo de entrada. certos??
                 sum += self.weights[index] * self.input[index]
+            if self.type is not input_layer:
+                sum += self.bias_weights[i]
             self.activia[i] = sigmoid(sum)
 
         return
@@ -75,18 +79,23 @@ class Layer:
 
         return
 
-    def gradient(self, deltas, weights):
-        ln = deltas.__len__()
-        for i in range(self.neuron_num):
-            delta = 0.0
-            for j in range(ln):
-                index = (j * self.neuron_num) + i
-                delta += deltas[j] * weights[index]
-
-            delta = delta * self.activia[i] * (1 - self.activia[i])
-            self.deltas[i] = delta
+    def gradient(self):
+        for i in range(self.inputs_num):
+            index = i//self.ins_per_neuron
+            self.gradients[i] = self.input[i] * self.deltas[index]
 
         return
+
+    def update(self):
+        for i in range(self.inputs_num):
+            self.weights[i] = self.weights[i] - (self.alpha * self.gradients[i])
+
+        #  update bias weights
+        for i in range(self.neuron_num):
+            self.bias_weights[i] = self.bias_weights[i] - (self.alpha * self.deltas[i])
+
+        return
+
 
 class NeuralNet:
     def __init__(self):
@@ -98,20 +107,22 @@ class NeuralNet:
         self.layers = []
         self.train = []
         self.test = []
+        self.alpha = 0.0
 
-    def initialize(self, dataset, neurons_input_layer, neurons_hidden_layer, neurons_output_layer, layer_num):
+    def initialize(self, dataset, neurons_input_layer, neurons_hidden_layer, neurons_output_layer, layer_num, alpha):
         self.dataset = dataset
         self.neurons_input_layer = neurons_input_layer
         self.neurons_hidden_layer = neurons_hidden_layer
         self.neurons_output_layer = neurons_output_layer
         self.layer_num = layer_num
+        self.alpha = alpha
 
-        self.layers.append(Layer(input_layer, self.neurons_input_layer, self.neurons_input_layer))
+        self.layers.append(Layer(input_layer, self.neurons_input_layer, self.neurons_input_layer, self.alpha))
         #  primeira hidden layer tem um número diferenciado de entradas
-        self.layers.append(Layer(hidden_layer, self.neurons_hidden_layer, self.neurons_input_layer))
+        self.layers.append(Layer(hidden_layer, self.neurons_hidden_layer, self.neurons_input_layer, self.alpha))
         for i in range(self.layer_num):
-            self.layers.append(Layer(hidden_layer, self.neurons_hidden_layer, self.neurons_hidden_layer * self.neurons_hidden_layer))
-        self.layers.append(Layer(output_layer, self.neurons_output_layer, self.neurons_hidden_layer * self.neurons_output_layer))
+            self.layers.append(Layer(hidden_layer, self.neurons_hidden_layer, self.neurons_hidden_layer * self.neurons_hidden_layer, self.alpha))
+        self.layers.append(Layer(output_layer, self.neurons_output_layer, self.neurons_hidden_layer * self.neurons_output_layer, self.alpha))
 
     def forward_feed(self, train_inst):  # função para propagar as entradas
         for i in range(self.neurons_input_layer):
@@ -127,4 +138,4 @@ class NeuralNet:
 neural_net = NeuralNet()
 
 if __name__ == '__main__':
-    neural_net.initialize('cancer.dat', 3, 5, 1, 2)
+    neural_net.initialize('cancer.dat', 3, 5, 1, 2, 0.5)
--- second_try/Test.py
+++ second_try/Test.py
@@ -2,6 +2,6 @@ num = 3333
 
 
 if __name__ == '__main__':
-    for i in range(5, 0, -1):
-        print(i)
+    for i in range(0, 8):
+        print(i//4)
 
